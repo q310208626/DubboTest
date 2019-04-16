1df348f8f6b9b764539e7a6b60cc50aa2dde5826
rentutu <15521385907@163.com>
2019-04-16 09:46:37 +0800

    Consuumer:修改ClassPathXml配置路径名称 Provider:增加System.in.read,让提供方持续运行

--- dubbodemoconsumer/src/main/java/com/lsj/test/consumer/Consumer.java
+++ dubbodemoconsumer/src/main/java/com/lsj/test/consumer/Consumer.java
@@ -5,12 +5,12 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Consumer {
     public static void main(String[] args) throws Exception {
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(new String[] {"META-INF/spring/dubbo-demo-consumer.xml"});
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(new String[] {"/applicationContext.xml"});
         context.start();
         // Obtaining a remote service proxy
         DemoService demoService = (DemoService)context.getBean("demoService");
         // Executing remote methods
-        String hello = demoService.sayHello("world");
+        String hello = demoService.sayHello("World");
         // Display the call result
         System.out.println(hello);
     }
