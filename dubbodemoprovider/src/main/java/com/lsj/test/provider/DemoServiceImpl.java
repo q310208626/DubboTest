8374c6a941dd068847485a2676dc08911ea5bec9
rentutu <15521385907@163.com>
2019-05-06 17:30:05 +0800

    配置文件添加注释，添加多个zk地址

--- dubbodemoprovider/src/main/java/com/lsj/test/provider/DemoServiceImpl.java
+++ dubbodemoprovider/src/main/java/com/lsj/test/provider/DemoServiceImpl.java
@@ -3,9 +3,15 @@ package com.lsj.test.provider;
 import com.lsj.test.api.DemoService;
 
 public class DemoServiceImpl implements DemoService {
+
     public String sayHello(String name) {
+
         String helloName = "";
-        if(null != name) helloName = name;
-        return "Hello"+helloName;
+
+        if(null != name) {
+            helloName = name;
+        }
+
+        return "Hello "+helloName;
     }
 }
